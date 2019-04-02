a4a0d4740f95a87ad2cf7c53355d341a44efc24a
Olosunde Kehinde Ademola <kennydudez2@gmail.com>
2019-04-02 11:39:16 +0100

    Worked on form validation, application controller, view and model

--- Angular/src/app/employees/employee/employee.component.ts
+++ Angular/src/app/employees/employee/employee.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { EmployeeService } from 'src/app/shared/employee.service';
 
 @Component({
   selector: 'app-employee',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: EmployeeService) { }
 
   ngOnInit() {
+    this.resetForm();
   }
-
+  resetForm(form? : NgForm) {      //making form parameter nullable 
+    if (form!=null)
+    form.resetForm();
+    this.service.FormData = {
+      EmployeeID : null,
+      FullName : '',
+     Position : '',
+      EMPCode : '',
+      Mobile : '',
+    }
+  } 
 }
